e86f8587f522d0bceaea8c1d834d3729775bd410
igor0073 <166950497+igor0073@users.noreply.github.com>
2024-06-21 12:58:59 +0300

    Add files via upload

---
+++ homework_5_1.py
@@ -0,0 +1,23 @@
+
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print('"Такого этажа не существует"')
+        else:
+            for i in range(1,new_floor + 1):
+                print(i)
+
+
+h1 = House('ЖК Эльбрус', 30)
+h2 = House('ТСЖ Карбышева', 5)
+print(h1.name, h1.number_of_floors)
+print(h2.name, h2.number_of_floors)
+h1.go_to(5)
+h2.go_to(7)
+
+
